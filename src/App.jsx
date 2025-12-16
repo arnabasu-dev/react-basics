f5ab0d3d182c5a2188aba1e8c2faaf1b0acabb58
Basu <abasu@bmc.com>
2025-12-16 08:59:14 +0530

    Intoducing useEffect and its dependency array and how they work with Side Effects in Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,26 @@
-import { useState } from "react"
-import Post from "./Posts"
-const App = ()=> {
-  let [post, setPost]= useState([{   //post is an array of objects which has the properties of a Component
-    name: "Mr Brown",
-    age: 2,
-    dp: "https://shorturl.at/8zw6P"
-  }]) 
+import { useEffect, useState } from "react"
 
-  //whats missing here is that the post array is not dynamic and needs to be updated in real time by adding more objects in the array.
-  // mapping postComponent to render <Post> Component in real time using the {post} state variable
-  let postComponent = post.map(p => <Post name={p.name}   //postComponent is created in real time as post changes which adds another component in the postComponent array.
-  age={p.age}
-  dp={p.dp}/>)
+const App = () => {
+  const [count, setCount] = useState(1)
 
-  const addPost = ()=> {
-    setPost([...post, {          // adding new objects to the post array will render the app and add another component into postComponent 
-      name: "Mr Tuffy",
-      age: 3,
-      dp: "https://shorturl.at/8zw6P"
-    }])
+  function increaseCount () {
+    setCount((prev)=> prev + 1)
   }
+
+  useEffect(()=> {
+    const clock = setInterval(increaseCount, 1000)}, [])
+  
+  useEffect(()=> {
+    const clock = setInterval(prev => prev -1);
+    
+    return ()=> {
+      clearInterval(clock)
+    }
+  }, [count])
+
   return <>
-    <div>
-      <button onClick={addPost}>Add</button>
-    </div>
-    {postComponent}
-    </>
+  {count} 
+  </>
 }
 
 export default App
